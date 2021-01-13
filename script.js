0db16b919c74f185d7e6b9b1c3c25fa4ff1012f3
Nour Rabea <75251467+Nour-rabea@users.noreply.github.com>
2021-01-13 15:41:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,129 +1,15 @@
-$(document).ready(function() {
+<iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRvDdkXVqTzxQ6x1LOsZ-d3dKwvqBhbIQp8D2rILYKU8TjiaelaTtxJo0EZYPRiuoIRILQFaV6WiXWC/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false" width="1050" height="500"></iframe>
 
-  var TITLE = 'مقارنه بين سعر المتر';
+# Analysis
+<iframe width="940" height="565" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRvDdkXVqTzxQ6x1LOsZ-d3dKwvqBhbIQp8D2rILYKU8TjiaelaTtxJo0EZYPRiuoIRILQFaV6WiXWC/pubchart?oid=187743359&amp;format=interactive"></iframe>
 
-  // `false` for vertical column chart, `true` for horizontal bar chart
-  var HORIZONTAL = false;
+<iframe width="940" height="530" seamless frameborder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vRvDdkXVqTzxQ6x1LOsZ-d3dKwvqBhbIQp8D2rILYKU8TjiaelaTtxJo0EZYPRiuoIRILQFaV6WiXWC/pubchart?oid=1549540456&amp;format=interactive"></iframe>
 
-	// `false` for individual bars, `true` for stacked bars
-  var STACKED = false;  
-  
-  // Which column defines 'bucket' names?
-  var LABELS = 'district';  
+# Map
+<iframe src="https://www.google.com/maps/d/embed?mid=1tf2QF4gb3HtwodAP9akl8tg7KmQNZMTH" width="940" height="680"></iframe>
 
-  // For each column representing a data series, define its name and color
-  var SERIES = [  
-    {
-      column: 'اقل سعر',
-      name: 'اقل سعر',
-      color: 'red'
-    },
-    {
-      column: 'اعلى سعر',
-      name: 'اعلى سعر',
-      color: 'blue'
-    }
-  ];
+# Presentation
+<iframe src="https://drive.google.com/file/d/13Lu5cP9YIS-6NJfesXr8Fr0BMF7U92DD/preview" width="900" height="490"></iframe>
 
-  // x-axis label and label in tooltip
-  var X_AXIS = 'المشاريع';
-
-  // y-axis label, label in tooltip
-  var Y_AXIS = 'سعر المتر';
-
-  // `true` to show the grid, `false` to hide
-  var SHOW_GRID = true; 
-
-  // `true` to show the legend, `false` to hide
-  var SHOW_LEGEND = true; 
-
-  // Read data file and create a chart
-  $.get('./data.csv', function(csvString) {
-
-    var rows = Papa.parse(csvString, {header: true}).data;
-
-    var datasets = SERIES.map(function(el) {
-      return {
-        label: el.name,
-        labelDirty: el.column,
-        backgroundColor: el.color,
-        data: []
-      }
-    });
-
-    rows.map(function(row) {
-      datasets.map(function(d) {
-        d.data.push(row[d.labelDirty])
-      })
-    });
-
-		var barChartData = {
-      labels: rows.map(function(el) { return el[LABELS] }),
-			datasets: datasets
-    };
-
-    var ctx = document.getElementById('container').getContext('2d');
-
-    new Chart(ctx, {
-      type: HORIZONTAL ? 'horizontalBar' : 'bar',
-      data: barChartData,
-      
-      options: {
-        title: {
-          display: true,
-          text: TITLE,
-          fontSize: 14,
-        },
-        legend: {
-          display: SHOW_LEGEND,
-        },
-        scales: {
-          xAxes: [{
-            stacked: STACKED,
-            scaleLabel: {
-              display: X_AXIS !== '',
-              labelString: X_AXIS
-            },
-            gridLines: {
-              display: SHOW_GRID,
-            },
-            ticks: {
-              beginAtZero: true,
-              callback: function(value, index, values) {
-                return value.toLocaleString();
-              }
-            }
-          }],
-          yAxes: [{
-            stacked: STACKED,
-            beginAtZero: true,
-            scaleLabel: {
-              display: Y_AXIS !== '',
-              labelString: Y_AXIS
-            },
-            gridLines: {
-              display: SHOW_GRID,
-            },
-            ticks: {
-              beginAtZero: true,
-              callback: function(value, index, values) {
-                return value.toLocaleString()
-              }
-            }
-          }]
-        },
-        tooltips: {
-          displayColors: false,
-          callbacks: {
-            label: function(tooltipItem, all) {
-              return all.datasets[tooltipItem.datasetIndex].label
-                + ': ' + tooltipItem.yLabel.toLocaleString();
-            }
-          }
-        }
-      }
-    });
-
-  });
-
-});
+## Copyrights
+  <iframe src="Nour.jpg"></iframe>
